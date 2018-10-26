c95309ed46dd5a419a794bb50b8032c976a60119
TSN Student <student@ulem.org>
2018-10-26 14:55:23 -0400

    2 cat clicker

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-document.querySelector("#cat").addEventListener("click", onClick);
-let counter = 0;
-const counterEle = document.querySelector("#counter");
+//ad eventListner for Cat and Ben image
 
-counterEle.textContent = counter;
-function onClick() {
-counter++;
-counterEle.textContent = counter;
+document.querySelector("#catimg").addEventListener("click", onClickCat);
+document.querySelector("#benimg").addEventListener("click", onClickBen);
+//Declare variables for Cat and Ben
+let counterCat = 0;
+let counterBen = 0;
+const counterEleCat = document.querySelector("#catCounter");
+const counterEleBen = document.querySelector("#benCounter");
+
+//Select counter element for both cat and ben
+counterEleCat.textContent = counterCat;
+counterEleBen.textContent = counterBen;
+function onClickCat() {
+counterCat++;
+counterEleCat.textContent = counterCat;
 }
+function onClickBen() {
+    counterBen++;
+    counterEleBen.textContent = counterBen;
+    }
