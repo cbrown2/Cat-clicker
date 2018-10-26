b98e8763c7a2729c8f3bfaf60f6484582a836240
TSN Student <student@ulem.org>
2018-10-26 14:05:47 -0400

    ok

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector("#cat").addEventListener("click", onClick);
+let counter = 0;
+const counterEle = document.querySelector("#counter");
+
+counterEle.textContent = counter;
+function onClick() {
+counter++;
+counterEle.textContent = counter;
+}
